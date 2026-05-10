feadceab527925835a1e9d7d058ab48090d8a3a7
vinodaraaj <gvinodaraaj@users.noreply.github.com>
2026-05-10 20:11:50 +0530

    revers

---
+++ Revers.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val kotlin = "myself is vinod hello"
+    val arrCh = kotlin.toCharArray()
+    var start=0
+    reversString(arrCh,start,arrCh.size-1)
+    arrCh.forEachIndexed(){i,ch->
+        if(ch==' ')
+        {
+            reversString(arrCh,start,i-1)
+            start=i+1
+        }
+    }
+    reversString(arrCh,start,arrCh.size-1)
+    println(arrCh.joinToString(""))
+}
+  
+fun reversString(arrCh:CharArray,start:Int,end:Int){
+    var left=start
+    var right=end
+    while(left<right){
+        val temp=arrCh[left]
+        arrCh[left]=arrCh[right]
+        arrCh[right]=temp
+        left++
+        right--
+    }
+}
