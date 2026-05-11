a48e715235a4477905ed48a438e60d3fc31eca18
vinodaraaj <gvinodaraaj@users.noreply.github.com>
2026-05-11 07:46:45 +0530

    Update FirstNonRepeating.kt
    string compres

--- FirstNonRepeating.kt
+++ FirstNonRepeating.kt
@@ -3,6 +3,28 @@ fun main() {
    
     println(firstNonRepeting2(kotlin))
 }
+fun main() {
+    val kotlin = "aaabbccccda"
+   
+    println(strCompres(kotlin))
+}
+fun strCompres(a: String): String {
+    if (a.isEmpty()) return ""
+    val result = StringBuilder()
+    var count = 1
+    for (i in 1 until a.length) {
+        if (a[i] == a[i - 1]) {
+            count++
+        } else {
+            result.append(a[i - 1])
+            result.append(count)
+            count = 1
+        }
+    }
+    result.append(a.last())
+    result.append(count)
+    return result.toString()
+}
  fun firstNonRepeting(a:String):String{
      val result=a.groupBy{it}.asSequence().firstOrNull{ it.value.size == 1 }
         ?.key
